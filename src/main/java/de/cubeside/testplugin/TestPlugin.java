9b5ce056c3a69766cdc51953e7c9fa2d2a17d888
Brokkonaut <hannos17@gmx.de>
2022-09-28 05:35:26 +0200

    Create testplugin

---
+++ src/main/java/de/cubeside/testplugin/TestPlugin.java
@@ -0,0 +1,73 @@
+package de.cubeside.testplugin;
+
+import de.cubeside.globalserver.event.Event;
+import de.cubeside.globalserver.event.EventHandler;
+import de.cubeside.globalserver.event.Listener;
+import de.cubeside.globalserver.plugin.Plugin;
+
+public class TestPlugin extends Plugin {
+    @Override
+    public void onLoad() {
+        getLogger().info("Hello World!");
+        getServer().getEventBus().registerHandlers(new TestListener());
+        getServer().getEventBus().dispatchEvent(new TestEvent("abc"));
+        getServer().getEventBus().dispatchEvent(new AdvancedTestEvent("def", 2));
+    }
+
+    @Override
+    public void onUnload() {
+        getLogger().info("Goodbye World!");
+    }
+
+    private class TestListener implements Listener {
+        @EventHandler(priority = -1000)
+        public void onEarlyTest(TestEvent event) {
+            getLogger().info("onEarlyTest: " + event.getValue());
+        }
+
+        @EventHandler(priority = 1000)
+        public void onLateTest(TestEvent event) {
+            getLogger().info("onLateTest: " + event.getValue());
+        }
+
+        @EventHandler
+        public void onTest(TestEvent event) {
+            getLogger().info("onTest: " + event.getValue());
+        }
+
+        @EventHandler
+        public void onAdvancedTest(AdvancedTestEvent event) {
+            getLogger().info("onAdvancedTest: " + event.getValue() + " -> " + event.getValue2());
+        }
+    }
+
+    public class TestEvent extends Event {
+        private String value;
+
+        public TestEvent(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public class AdvancedTestEvent extends TestEvent {
+        private int value2;
+
+        public AdvancedTestEvent(String value, int value2) {
+            super(value);
+            this.value2 = value2;
+        }
+
+        @Override
+        public String getValue() {
+            return "Advanced" + super.getValue();
+        }
+
+        public int getValue2() {
+            return value2;
+        }
+    }
+}
